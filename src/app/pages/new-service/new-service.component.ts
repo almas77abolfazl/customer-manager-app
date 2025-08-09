2560936020603811e36f61a248eca5ddc4dc506a
Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>
2025-08-10 00:40:51 +0330

    feat: add new service section
    Signed-off-by: Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>

--- src/app/pages/new-service/new-service.component.ts
+++ src/app/pages/new-service/new-service.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-service',
   templateUrl: './new-service.component.html',
   styleUrls: ['./new-service.component.scss'],
   standalone: true,
+  imports: [IonicModule, FormsModule, CommonModule]
 })
-export class NewServiceComponent  implements OnInit {
+export class NewServiceComponent {
+  customerSearch = '';
+  filteredCustomers: any[] = [];
+  customers = [
+    { id: 1, name: 'رضا موسوی' },
+    { id: 2, name: 'علی رضایی' },
+    { id: 3, name: 'حسین کریمی' }
+  ];
 
-  constructor() { }
+  showNewCustomerForm = false;
+  newCustomer = { name: '', phone: '' };
 
-  ngOnInit() {}
+  selectedCustomer: any = null;
+  service = { type: '', price: null, notes: '' };
 
+  searchCustomers() {
+    const term = this.customerSearch.trim();
+    this.filteredCustomers = this.customers.filter(c =>
+      c.name.includes(term)
+    );
+  }
+
+  selectCustomer(customer: any) {
+    this.selectedCustomer = customer;
+    this.showNewCustomerForm = false;
+    this.customerSearch = customer.name;
+    this.filteredCustomers = [];
+  }
+
+  createCustomer() {
+    if (this.newCustomer.name) {
+      const newCust = {
+        id: this.customers.length + 1,
+        ...this.newCustomer
+      };
+      this.customers.push(newCust);
+      this.selectCustomer(newCust);
+      this.newCustomer = { name: '', phone: '' };
+    }
+  }
+
+  saveService() {
+    console.log('سرویس ذخیره شد', {
+      customer: this.selectedCustomer,
+      service: this.service
+    });
+  }
 }
